597d0d7e619593cabba310948c3f4922ab7d3f8b
InGrowth <gimdongwoo@gmail.com>
2017-02-08 19:24:48 +0900

    bash shell script bug fix

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,14 +4,14 @@ MONGODB1=`ping -c 1 mongo1 | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
 MONGODB2=`ping -c 1 mongo2 | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
 MONGODB3=`ping -c 1 mongo3 | head -1  | cut -d "(" -f 2 | cut -d ")" -f 1`
 
-echo "Waiting for startup.."
-until curl http://${MONGODB1}:28017/serverStatus\?text\=1 2>&1 | grep uptime | head -1; do
+echo "Waiting for MongoDB startup.."
+until [ "$(curl http://${MONGODB1}:28017/serverStatus\?text\=1 2>&1 | grep uptime | head -1)" ]; do
   printf '.'
   sleep 1
 done
 
-echo curl http://${MONGODB1}:28017/serverStatus\?text\=1 2>&1 | grep uptime | head -1
-echo "Started.."
+echo $(curl http://${MONGODB1}:28017/serverStatus\?text\=1 2>&1 | grep uptime | head -1)
+echo "MongoDB Started.."
 
 
 echo SETUP.sh time now: `date +"%T" `
