4921cf014358af24dd1d1986d4fa4e484e78b139
InGrowth <gimdongwoo@gmail.com>
2017-02-11 07:29:22 +0900

    port change

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -19,7 +19,6 @@ echo SETUP.sh time now: `date +"%T" `
 mongo --host mongo1:27017 <<EOF
    var cfg = {
         "_id": "rs0",
-        "version": 1,
         "members": [
             {
                 "_id": 0,
@@ -28,12 +27,12 @@ mongo --host mongo1:27017 <<EOF
             },
             {
                 "_id": 1,
-                "host": "mongo2:27017",
+                "host": "mongo2:27019",
                 "priority": 0
             },
             {
                 "_id": 2,
-                "host": "mongo3:27017",
+                "host": "mongo3:27018",
                 "priority": 1,
                 "arbiterOnly": true
             }
