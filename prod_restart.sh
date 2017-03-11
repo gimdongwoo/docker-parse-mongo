9bcd8a2c2bf578ed0d5f76f0802f3f3f068b5ecb
InGrowth <gimdongwoo@gmail.com>
2017-03-11 12:45:47 +0900

    change

--- prod_restart.sh
+++ prod_restart.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-echo "RESTART parseapi1"
-docker-compose restart parseapi1
-
-echo "RESTART parseapi2"
-docker-compose restart parseapi2
+echo "RESTART parseapi"
+docker-compose restart parseapi
 
 echo "RESTART DONE"
 docker-compose logs -f --tail=100
