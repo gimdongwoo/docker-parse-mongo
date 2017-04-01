41419179eaf63c426de1adf7786102cd57c5c157
InGrowth <gimdongwoo@gmail.com>
2017-04-01 19:29:40 +0900

    start.sh fix

--- prod_start.sh
+++ prod_start.sh
@@ -3,8 +3,11 @@
 echo "BUILD DOCKER"
 docker-compose build
 
+echo "START DOCKER"
+docker-compose up -d
+
 echo "REMOVE UNUSED IMAEGE"
 docker rmi $(docker images -q -f dangling=true)
 
-echo "START DOCKER"
-docker-compose up -d && docker-compose logs -f
+echo "DOCKER LOGS"
+docker-compose logs -f
